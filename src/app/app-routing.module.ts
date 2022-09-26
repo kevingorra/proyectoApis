59cec68079b9ac5e9a4aee715651424f98f38372
CristianP50 <cristo>
2022-09-26 12:24:52 -0500

    iniciamos proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { MercanciaComponent } from './mercancia/mercancia.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent,pathMatch:"full"},
+  {path:"mercancia",component:MercanciaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
